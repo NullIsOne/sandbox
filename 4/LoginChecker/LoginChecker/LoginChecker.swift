f917e391b5127739054e97651fc786122ab1d55d
Nikita Korobeinikov <n.korobeinikov@nosorog-studio.com>
2020-07-24 14:27:47 +0400

    #4 add checker realisation on regex (green)

--- 4/LoginChecker/LoginChecker/LoginChecker.swift
+++ 4/LoginChecker/LoginChecker/LoginChecker.swift
@@ -7,7 +7,26 @@
 //
 
 struct LoginChecker {
+    
+    private static let allowedStartCharactersRegex = try! NSRegularExpression(pattern: "[A-Za-z]")
+    private static let allowedCharactersRegex = try! NSRegularExpression(pattern: "[A-Za-z0-9@.-]")
+        
     static func isValid(login: String) -> Bool {
-        return false
+        guard login.count >= 3, login.count <= 32 else {
+            return false
+        }
+        
+        let allowedStartCharactersMatch = allowedStartCharactersRegex.firstMatch(in: login,
+                                                                                 options: [],
+                                                                                 range: NSRange(location: 0, length: 1))
+        guard allowedStartCharactersMatch != nil else {
+            return false
+        }
+        
+        let allowedCharactersMatches = allowedCharactersRegex.matches(in: login,
+                                                                      options: [],
+                                                                      range: NSRange(location: 0,
+                                                                                     length: login.count))
+        return allowedCharactersMatches.count == login.count
     }
 }
